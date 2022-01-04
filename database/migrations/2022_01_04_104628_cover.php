e86abd5c5fc53f2407aa6ff0018483e7c5d2eb36
Denis <webconsul@yandex.ru>
2022-01-04 13:57:21 +0300

    migrations

---
+++ database/migrations/2022_01_04_104628_cover.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Cover extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('covers', function (Blueprint $table) {
+            $table->id();
+            $table->string('model');
+            $table->string('folder');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('covers');
+    }
+}
